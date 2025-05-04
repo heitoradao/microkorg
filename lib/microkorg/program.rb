1c57ae392418ba6a6db7dea074bacb131eeeb2d6
HeitorJr <heitor.adao@gmail.com>
2025-05-04 16:54:58 -0300

    Add program class

---
+++ lib/microkorg/program.rb
@@ -0,0 +1,18 @@
+
+class Program #< BinData::Record
+  def init
+  end
+
+  def name
+    'Init Program'
+  end
+
+  def mixer
+    [127, 0, 0]
+  end
+
+  def osc1
+    [1, 2, 3, 4, 5]
+  end
+end
+
