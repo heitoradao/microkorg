5d38e9c6f59eae56355c3841f73aeb56578873bb
HeitorJr <heitor.adao@gmail.com>
2025-08-14 20:11:55 -0300

    console + spec

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Microkorg::Program do
 
     expect(p.header).to eq("MK2PROG\x00")
     expect(p.name.delete("\x00")).to eq("Init Program".force_encoding('ASCII-8BIT'))
+    expect(p.tempo).to eq(120)
     #expect(p.valid?).to eq(true)
     #expect(p.filename).to eq('U_A1_Init_Program.mk2prog')
   end
